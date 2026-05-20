7b35a20274366b663c6cf24d48f639139923e4ff
Tehran Hamidli <110116178+TehranHamidli@users.noreply.github.com>
2026-05-20 16:36:22 +0400

    largest word string

--- strings/largest-word-string/main.cpp
+++ strings/largest-word-string/main.cpp
@@ -8,23 +8,27 @@ int main()
 
     getline(cin, text);
 
-    // C++ da corek yoxdur.
-
-    if (text.empty())
-        return 0;
-
-    bool newWord = true;
-    int wordCount = 0;
-
-    cout << text << endl;
+    string word = "";
+    string maxWord = "";
 
     for (size_t i = 0; i < text.length(); ++i)
     {
 
-        if (newWord)
-        {   
-            cout << text[i] << endl;
-            wordCount++;
+        if (text[i] == ' ' || text[i] == '.')
+        {
+
+            if ( word.length() > maxWord.length())
+            {
+                maxWord = word;
+            }
+
+            word = ""; 
+        }
+        else
+        {
+            word += text[i];
         }
     }
+
+    cout << maxWord ;
 }
