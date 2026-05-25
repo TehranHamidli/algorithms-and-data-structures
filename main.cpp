0ef0912364d1cbb1486cb15063161e7e7a6effcb
Tehran Hamidli <110116178+TehranHamidli@users.noreply.github.com>
2026-05-25 15:02:30 +0400

    main.cpp empty

--- main.cpp
+++ main.cpp
@@ -6,21 +6,5 @@ using namespace std;
 int main()
 {
 
-    string numberStrings;
-
-    getline(cin, numberStrings);
-
-    for (size_t i = 0; i < numberStrings.length(); ++i)
-    {
-
-        cout << numberStrings[i];
-
-            if (numberStrings[i] == numberStrings[i + 1])
-                continue;
-
-            if (numberStrings[i] % 2 != 0 && numberStrings[i + 1] % 2 != 0)
-            {
-                cout << "-";
-            }
-    }
+ 
 }
