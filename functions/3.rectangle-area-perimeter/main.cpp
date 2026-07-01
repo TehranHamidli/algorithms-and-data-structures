e48e624d54453413719442a4bc171472d14feb2b
Tehran Hamidli <110116178+TehranHamidli@users.noreply.github.com>
2026-07-01 15:09:45 +0400

    reactangle area and perimeter

--- functions/3.rectangle-area-perimeter/main.cpp
+++ functions/3.rectangle-area-perimeter/main.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
 
-
 using namespace std;
 
+void calculateAreaRectangle(int length, int width)
+{
 
-int main () {
-    
+    int area = length * width;
+
+    cout << "The area of the rectangle is: " << area << endl;
+}
+
+void calculatePerimeterRectangle(int length, int width)
+{
+
+    int perimeter = 2 * (length + width);
+
+    cout << "The perimeter of the rectangle is: " << perimeter << endl;
+}
+
+int main()
+{
+
+    int l, w;
+
+    cin >> l >> w;
+
+    calculateAreaRectangle(l, w);
+    calculatePerimeterRectangle(l, w);
 }
